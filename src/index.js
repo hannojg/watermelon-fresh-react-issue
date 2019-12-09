f70ceeccff32c61bafff9de5432bcba3494cd81b
Hanno Gödecke <h.gdk@cernischi-und-goedecke.de>
2019-12-09 19:28:17 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import LokiJSAdapter from "@nozbe/watermelondb/src/adapters/lokijs";
+import {Database} from "@nozbe/watermelondb/";
+import {mySchema} from "./models/schema";
+import Blog from "./models/Blog";
+import Post from "./models/Post";
+import Comment from "./models/Comment";
+
+
+const adapter = new LokiJSAdapter({
+    dbName: 'WatermelonDemo',
+    schema: mySchema,
+});
+
+const database = new Database({
+    adapter,
+    modelClasses: [Blog, Post, Comment],
+    actionsEnabled: true,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
